cc75fbcf28a2ec460c856eedec521eebbff800db
LoveBettygirl <bettycyygirl@163.com>
2021-12-25 20:55:53 +0800

    Add connection pool

---
+++ src/mysql/mysql_connection_pool.cpp
@@ -0,0 +1,129 @@
+#include <mysql/mysql.h>
+#include <string>
+#include <cstdlib>
+#include <list>
+#include <pthread.h>
+#include <iostream>
+#include "mysql_connection_pool.h"
+
+using namespace std;
+
+ConnectionPool::ConnectionPool() : mCurConn(0), mFreeConn(0) {}
+
+ConnectionPool *ConnectionPool::getInstance()
+{
+    static ConnectionPool connPool;
+    return &connPool;
+}
+
+void ConnectionPool::init(const string &url, const string &user, const string &passWord, const string &databaseName, int port, int maxConn, int closeLog)
+{
+    mUrl = url;
+    mPort = port;
+    mUser = user;
+    mPassWord = passWord;
+    mDatabaseName = databaseName;
+    mCloseLog = closeLog;
+
+    for (int i = 0; i < maxConn; i++) {
+        MYSQL *con = nullptr;
+        con = mysql_init(con);
+
+        if (con == nullptr) {
+            LOG_ERROR("MySQL Error");
+            exit(MYSQL_ERROR);
+        }
+        con = mysql_real_connect(con, url.c_str(), user.c_str(), passWord.c_str(), databaseName.c_str(), port, nullptr, 0);
+
+        if (con == nullptr) {
+            LOG_ERROR("MySQL Error");
+            exit(MYSQL_ERROR);
+        }
+        connList.push_back(con);
+        ++mFreeConn;
+    }
+
+    reserve = Semaphore(mFreeConn);
+
+    mMaxConn = mFreeConn;
+}
+
+
+// 当有请求时，从数据库连接池中返回一个可用连接，更新使用和空闲连接数
+MYSQL *ConnectionPool::getConnection()
+{
+    MYSQL *con = nullptr;
+
+    if (0 == connList.size())
+        return nullptr;
+
+    reserve.wait();
+
+    lock.lock();
+
+    con = connList.front();
+    connList.pop_front();
+
+    --mFreeConn;
+    ++mCurConn;
+
+    lock.unlock();
+    return con;
+}
+
+// 释放当前使用的连接
+bool ConnectionPool::releaseConnection(MYSQL *con)
+{
+    if (con == nullptr)
+        return false;
+
+    lock.lock();
+
+    connList.push_back(con);
+    ++mFreeConn;
+    --mCurConn;
+
+    lock.unlock();
+
+    reserve.post();
+    return true;
+}
+
+// 销毁数据库连接池
+void ConnectionPool::destroyPool()
+{
+    lock.lock();
+    if (connList.size() > 0) {
+        list<MYSQL *>::iterator it;
+        for (it = connList.begin(); it != connList.end(); ++it) {
+            MYSQL *con = *it;
+            mysql_close(con);
+        }
+        mCurConn = 0;
+        mFreeConn = 0;
+        connList.clear();
+    }
+    lock.unlock();
+}
+
+// 当前空闲的连接数
+int ConnectionPool::getFreeConn()
+{
+    return this->mFreeConn;
+}
+
+ConnectionPool::~ConnectionPool()
+{
+    destroyPool();
+}
+
+ConnectionRAII::ConnectionRAII(MYSQL **SQL, ConnectionPool *connPool){
+    *SQL = connPool->getConnection();
+
+    conRAII = *SQL;
+    poolRAII = connPool;
+}
+
+ConnectionRAII::~ConnectionRAII(){
+    poolRAII->releaseConnection(conRAII);
+}
---
+++ src/mysql/mysql_connection_pool.h
@@ -0,0 +1,58 @@
+#ifndef CONNECTION_POOL_H
+#define CONNECTION_POOL_H
+
+#include <cstdio>
+#include <list>
+#include <mysql/mysql.h>
+#include <error.h>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include "../thread/locker.h"
+#include "../log/log.h"
+#include "../common.h"
+
+using namespace std;
+
+class ConnectionPool {
+public:
+    MYSQL *getConnection(); // 获取数据库连接
+    bool releaseConnection(MYSQL *conn); // 释放连接
+    int getFreeConn(); // 获取连接
+    void destroyPool(); // 销毁所有连接
+
+    static ConnectionPool *getInstance();
+
+    void init(const string &url, const string &user, const string &passWord, const string &databaseName, int port, int maxConn, int closeLog); 
+
+private:
+    ConnectionPool();
+    ConnectionPool();
+
+    int mMaxConn; // 最大连接数
+    int mCurConn; // 当前已使用的连接数
+    int mFreeConn; // 当前空闲的连接数
+    Locker lock;
+    list<MYSQL *> connList; // 连接池
+    Semaphore reserve;
+
+public:
+    string mUrl; // 主机地址
+    string mPort; // 数据库端口号
+    string mUser; // 登陆数据库用户名
+    string mPassWord; // 登陆数据库密码
+    string mDatabaseName; // 使用数据库名
+    int mCloseLog; // 日志开关
+};
+
+class ConnectionRAII {
+public:
+    ConnectionRAII(MYSQL **con, ConnectionPool *connPool);
+    ~ConnectionRAII();
+    
+private:
+    MYSQL *conRAII;
+    ConnectionPool *poolRAII;
+};
+
+#endif
